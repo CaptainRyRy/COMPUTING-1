0585f7b8f70f6a33413a7e60bf5ca3d98603bc20
CaptainRyRy <riley.soto@gmail.com>
2017-12-18 19:37:50 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,28 @@
+/*
+Title: Anti-Capitalist Computing: Ternary Code and Fighting Fascists.
+Imagined, Designed, and Programmed by: Riley Soto
+Date: 12-15-2017
+Description: Learn a little bit about Setun, the ternary computer made in Moscow State University, and follow it up with most Soviet of activities: Fighting fascists!
+Sources of ideas and inspiration (title, author, URL):
+ * 
+ *
+ *
+ 
+Includes code from (title, author, URL):
+ * 
+ * 
+ *
+ 
+<Reminders, you can delete:>
+<Use command + T to auto-indent your code for cleaner formatting>
+<Check that variable and function names are easy to read and understand. eg. "squareButton" vs.
+"shapeButton2">
+<If you can't remember where you found some code, Google the text to find it.>
+<Put your name in the name of this file!>
+*/
+
+
+//THIS IS THE PREVIOUS ITERATION OF THE PROJECT, KEPT IT FOR POSTERITY
 // var song;
 // var lenin;
 // var badasssovietwomansoldier;
@@ -37,6 +62,13 @@
 //asteroid clone (core mechanics only)
 //arrow keys to move + x to shoot
 
+
+
+
+
+
+//NOW HERE'S THE ACTION CODE:
+
 var bullets;
 var asteroids;
 var ship;
@@ -45,11 +77,13 @@ var MARGIN = 20;
 
 function setup() {
 createCanvas(1600,820);
-
+  
+//MADE MY OWN IMAGES FOR THE SPRITES FOR THE GAME!
 bulletImage = loadImage("assets/asteroids_bullet.png");
 shipImage = loadImage("assets/asteroids_ship0001.png");
 particleImage = loadImage("assets/asteroids_particle.png");
 
+//HERE ARE SETTING THE ATTRIBUTES OF THE SHIP ITSELF (THE SHIP WHICH DOES NOT MOVE UNLESS HIT BY THE ASTEROIDS)
 ship = createSprite(width/2, height/2);
 ship.maxSpeed = 6;
 ship.friction = .9;
@@ -58,9 +92,11 @@ ship.setCollider("circle", 0,0, 20);
 ship.addImage("normal", shipImage);
 ship.addAnimation("thrust", "assets/asteroids_ship0001.png", "assets/asteroids_ship0002.png");
 
+
 asteroids = new Group();
 bullets = new Group();
 
+//THIS SECTION CREATES NEW SMALLER ASTEROIDS AFTER HITTING A LARGER ONE, TELLING IT TO FORM TWO OF THE NEXT LEVEL DOWN AT A RANDOM ANGLE
 for(var i = 0; i<8; i++) {
   var ang = random(360);
   var px = width/2 + 1000 * cos(radians(ang));
@@ -72,11 +108,13 @@ for(var i = 0; i<8; i++) {
 function draw() {
   background(81,120,48);
 
+  //THEN WE HAVE THE INSTRUCTION TEXT AT THE TOP...
   fill(0);
   textAlign(CENTER);
   textSize(36);
   text("Your treads have been disabled by the fascists! Defend the Union! Controls: Left Arrow Key + X", width/2, 40);
 
+  //THIS SECTION TELLS SPTIRES WHERE TO APPEAR ON THE OPPOSITE SIDE WHEN THEY MOVE OFF OF THE MARGIN OF THE GAME
   for(var i=0; i<allSprites.length; i++) {
   var s = allSprites[i];
   if(s.position.x<-MARGIN) s.position.x = width+MARGIN;
@@ -89,6 +127,7 @@ function draw() {
 
   ship.bounce(asteroids);
 
+  //HERE WE HAVE THE SHIP CONTROLS, NOTICE HOW I MADE THE ROTATION FOR THE RIGHT KEY NEGATIVE, MEANING THE SHIP CAN ONLY TURN LEFT! I THOUGHT THAT WAS A FUNNY JOKE TO PUT IN.
   if(keyDown(LEFT_ARROW))
     ship.rotation -= 2;
   if(keyDown(RIGHT_ARROW))
@@ -101,6 +140,7 @@ function draw() {
   else
     ship.changeAnimation("normal");
 
+  //AND THEN WE HAVE THE ACTUAL FIRING OF THE BULLET, THE VARIABLES CHANGE HOW FAST IT TRAVELS (I MADE IT RELATIVELY FAST) AND HOW LONG IT CAN EXIST AFTER LEAVING THE BARREL.
   if(keyWentDown("x"))
     {
     var bullet = createSprite(ship.position.x, ship.position.y);
@@ -113,7 +153,7 @@ function draw() {
   drawSprites();
 
 }
-
+//AND HERE IS THE ACTUAL CODE FOR THE ASTEROIDS, THEIR ROTATION SPEED (WHICH I SLOWED DOWN A LOT) AND THEIR TRAVELLING SPEED AND THE SIZE OF EACH SMALLER LEVEL OF ASTEROID.
 function createAsteroid(type, x, y) {
   var a = createSprite(x, y);
   var img  = loadImage("assets/asteroids_particle.png");
@@ -123,17 +163,20 @@ function createAsteroid(type, x, y) {
   //a.debug = true;
   a.type = type;
 
+  //YES, HERE ARE THE SMALLER SIZES.
   if(type == 2)
     a.scale = .6;
   if(type == 1)
     a.scale = .3;
 
+  //THIS SETS HOW MUCH MASS THEY HAVE, SO WITH HOW MUCH FORCE THEY BUMP INTO THE SHIP.
   a.mass = 2+a.scale;
   a.setCollider("circle", 0, 0, 50);
   asteroids.add(a);
   return a;
 }
 
+//THIS MAKES IT SO THAT WHEN AN ASTEROID IS IT THE NEXT TYPE WILL BE ONE SMALLER. I EXPLORED THIS SECTION A LOT AND HAD SOME CRAZY RESULTS.
 function asteroidHit(asteroid, bullet) {
 var newType = asteroid.type-1;
 
@@ -142,6 +185,7 @@ if(newType>0) {
   createAsteroid(newType, asteroid.position.x, asteroid.position.y);
   }
 
+//THIS SETS THE RANDOM SPEED FOR THE ASTEROIDS AT THE VERY BEGINNING, ALONG WITH THE FRICTION THEY HAVE (THEY EVENTUALLY SLOW DOWN)
 for(var i=0; i<10; i++) {
   var p = createSprite(bullet.position.x, bullet.position.y);
   p.addImage(particleImage);
